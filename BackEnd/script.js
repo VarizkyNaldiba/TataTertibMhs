7bfdc9c3cbdf7c2fef80d30168420e8c26b1516e
soulqan <soultan.agnar@gmail.com>
2024-11-12 11:23:56 +0700

    list Tatib

--- BackEnd/script.js
+++ BackEnd/script.js
@@ -15,3 +15,19 @@ function showPage(page) {
     var clickedItem = document.querySelector(`.sidebar li a[onclick="showPage('${page}')"]`).parentNode;
     clickedItem.classList.add('active');
   }
+
+  //script list tatib
+        document.getElementById('filter-tingkat').addEventListener('change', function () {
+            const filterValue = this.value;
+            const rows = document.querySelectorAll('#tatib-table tbody tr');
+
+            rows.forEach(row => {
+                const tingkat = row.getAttribute('data-tingkat');
+                if (filterValue === "" || filterValue === tingkat) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+        });
+  
