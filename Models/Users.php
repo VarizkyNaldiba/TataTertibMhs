58f94b5184fb18e954a4d8afa5b2b09867a10420
GhoffarFitassin <ghoffar8804@gmail.com>
2024-11-28 19:39:20 +0700

    update: Login and Logout fitur

---
+++ Models/Users.php
@@ -0,0 +1,4 @@
+<?
+require_once '../config.php';
+$sql = "SELECT * FROM mahasiswa UNION ALL SELECT * FROM dosen";
+?>
--- views/pelanggaranpage.php
+++ views/pelanggaranpage.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+// session_destroy();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: login.php");
+    exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,6 +39,7 @@
             <li><a href="listTatib.php"><i class="fa-solid fa-book"></i></a></li>
             <li class="active"><a href="pelanggaranpage.php"><i class="fa-solid fa-hand"></i></a></li>
             <li><a href="notifikasi_dosen.php"><i class="fa-solid fa-bell"></i></a></li>
+            <li><a href="?logout=true"><i class="fa-solid fa-right-from-bracket"></i></a></li>
         </ul>
     </div>
 
