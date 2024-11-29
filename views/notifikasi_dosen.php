4fb95e4593683e69f486d47b90dac9361a1d57b6
soulqan <soultan.agnar@gmail.com>
2024-11-29 07:43:24 +0700

    logoout button

--- views/notifikasi_dosen.php
+++ views/notifikasi_dosen.php
@@ -21,6 +21,7 @@
             <li><a href="listTatib.php"><i class="fa-solid fa-book"></i></a></li>
             <li><a href="pelanggaranpage.php"><i class="fa-solid fa-hand"></i></i></a></li>
             <li class="active"><a href="notifikasi_dosen.php"><i class="fa-solid fa-bell"></i></a></li>
+            <li class="logout"><a href="?logout=true"><i class="fa-solid fa-right-from-bracket"></i></a></li>
     </ul>
 </div>
     <div class="content">
