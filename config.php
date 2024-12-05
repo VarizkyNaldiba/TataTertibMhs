1ee6e74015e8b2f9131e81f03d6c8a69cf0d18ca
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2024-12-05 21:55:10 +0700

    perbaikan kesalahan file merge

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-function connectToDatabase($servername, $dbname) {
+function connectToDatabase($servername, $dbname, $username, $password) {
     try {
-        $conn = new PDO("sqlsrv:Server=$servername;Database=$dbname");
+        $conn = new PDO("sqlsrv:Server=$servername;Database=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "con berhasil";
+        echo "Koneksi berhasil";
         return $conn;
     } catch(PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
+        die("Koneksi gagal: " . $e->getMessage());
     }
 }
 
-$connect = connectToDatabase("LAPTOP-CEEMFUHE", "DiscipLink");
+$connect = connectToDatabase("LAPTOP-VMVSF6A7\PBO", "DiscipLink", "sa", "12345");
 ?>
