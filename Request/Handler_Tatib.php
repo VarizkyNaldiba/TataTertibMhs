9f83126aa144aa9b1b1d4d5fa4d35b88bdaeecf0
GhoffarFitassin <ghoffar8804@gmail.com>
2024-12-12 09:28:23 +0700

    update: insert delete tatib v1.0

---
+++ Request/Handler_Tatib.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require_once '../config.php';
+require_once '../Controllers/TatibController.php';
+
+
+$tatibController = new TatibController();
+
+
+
+if(isset($_POST['store']) && isset($_POST['admin']) && isset($_POST['deskripsi']) && isset($_POST['tingkat']) && isset($_POST['poin'])) {
+    $tatibController->store(
+        $_POST['admin'],
+        $_POST['deskripsi'],
+        $_POST['tingkat'],
+        $_POST['poin']
+    );
+} else if(isset($_POST['update']) && isset($_POST['deskripsi']) && isset($_POST['tingkat']) && isset($_POST['poin'])) {
+    $tatibController->update(
+        $_POST['id_tatib'],
+        $_POST['deskripsi'],
+        $_POST['tingkat'],
+        $_POST['poin']
+    );
+} else if(isset($_POST['delete']) && isset($_POST['id_tatib'])) {
+    $tatibController->delete($_POST['id_tatib']);
+}
+
+header("Location: ../views/listTatib-admin.php");
+exit();
+
+?>
